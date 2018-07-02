e3a917db5fee383d35f68b71cd8ef8fb63741b1a
MouseKI <347702526q@gmail.com>
2018-07-02 15:51:36 -0700

    upload the update json file

--- TagAPI/src/TagAPI/TagAPI.java
+++ TagAPI/src/TagAPI/TagAPI.java
@@ -1,5 +1,34 @@
 package TagAPI;
 
-public class TagAPI {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class TagAPI {
+	public static void readFromWeb(String webURL) throws IOException {
+        URL url = new URL(webURL);
+        InputStream is =  url.openStream();
+        try( BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new MalformedURLException("URL is malformed!!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException();
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        String url = "https://keliang.myshopify.com/admin/products";
+        readFromWeb(url);
+    }
 }
